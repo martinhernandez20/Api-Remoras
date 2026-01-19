5d6e9a47d2c39d1a63574105fc035573085a5e46
martinhernandez20 <martin20.edge@gmail.com>
2026-01-19 16:25:28 -0300

    Initialize Express server with basic route

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Remoras funcionando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
