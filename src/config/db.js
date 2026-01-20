015f4852262e8aa1641efd00f05ea4722c6a7a32
martinhernandez20 <martin20.edge@gmail.com>
2026-01-20 09:52:13 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,20 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+      }
+);
 
 module.exports = pool;
